742e556122846e305c169eb6a958e4ccd0c2b2b0
tsalvia <almtr.ctf@gmail.com>
2019-05-22 23:44:51 +0900

    [Add] コマンドオプションを指定できるようにした

--- json2csv.js
+++ json2csv.js
@@ -3,6 +3,8 @@
 const jQueryCSV = require('jquery-csv');
 const fs = require('fs');
 const path = require('path');
+const commandLineArgs = require('command-line-args');
+const commandLineUsage = require('command-line-usage');
 
 function getType(obj) {
     return Object.prototype.toString.call(obj).slice(8, -1);
@@ -23,7 +25,7 @@ function parse_object(obj, path) {
         }
         return d;
     }
-    
+
     if (scalar) {
         const d = {};
         const endPath = path.substr(0, path.length - 1);
@@ -45,14 +47,112 @@ function getFileNameWithoutExt(pathstr) {
     return path.basename(pathstr, path.extname(pathstr));
 }
 
-if (process.argv.length == 3) {
-    const inputFile = process.argv[2];
-    const outputFile = getFileNameWithoutExt(inputFile) + '.csv';
-
-    const json = require(inputFile);
-    const csv = json2csv(json);
-    const charset = 'utf8';
-    fs.writeFileSync(outputFile, csv, charset);
-} else {
-    console.log('Usage: node json2csv.js <file>');
+function isExistFile(file) {
+    try {
+        fs.accessSync(file);
+    } catch (err) {
+        if (err.code === 'ENOENT')
+            return false;
+    }
+    return true;
 }
+
+function printError(msg) {
+    console.error('Error: ' + msg);
+    console.error('For more information, try to use the \'--help\' or \'-h\' option to show help.');
+}
+
+const optionDefinitions = [
+    {
+        name: 'input',
+        alias: 'i',
+        type: String,
+        description:
+            'This option is required.\n' +
+            'Specifies the JSON file you want to convert to CSV format.'
+    },
+    {
+        name: 'output',
+        alias: 'o',
+        type: String,
+        description: 'Specifies the file name for the output. The default is "input file name".csv'
+    },
+    {
+        name: 'help',
+        alias: 'h',
+        type: Boolean,
+        description: 'Display specific types of command line options.'
+    }
+];
+
+const sections = [
+    {
+        header: 'json2csv',
+        content: 'Convert JSON format files to CSV format.'
+    },
+    {
+        header: 'Options',
+        optionList: optionDefinitions
+    },
+    {
+        header: 'Examples',
+        content: [
+            {
+                desc: '1. Example of specifying input files only',
+                example: '$ node json2csv.js --input Sample.json'
+            },
+            {
+                desc: '2. Example of specifying output destination.',
+                example: '$ node json2csv.js --input Sample.json --output output/Sample.csv'
+            }
+        ]
+    },
+];
+
+let options;
+try {
+    options = commandLineArgs(optionDefinitions);
+} catch (err) {
+    switch (err.name) {
+        case 'UNKNOWN_OPTION':
+            printError('Unrecoginzed option \'' + err.optionName + '\'');
+            break;
+        case 'UNKNOWN_VALUE':
+            printError('Unrecoginzed value \'' + err.value + '\'');
+            break;
+        default:
+            printError('\'' + err.name + '\' is unknown error.');
+            break;
+    }
+    process.exit();
+}
+
+if (options.help) {
+    const usage = commandLineUsage(sections);
+    console.log(usage);
+    process.exit();
+}
+
+if (!options.input) {
+    printError('No JSON file specified.');
+    process.exit();
+}
+
+const inputFile = options.input;
+if (!isExistFile(inputFile)) {
+    printError('\'' + inputFile + '\': No such file or directory.');
+    process.exit();
+}
+
+let outputFile = options.output;
+if (!outputFile)
+    outputFile = getFileNameWithoutExt(inputFile) + '.csv';
+
+const outputDir = path.dirname(outputFile);
+if (!fs.existsSync(outputDir))
+    fs.mkdirSync(outputDir, { recursive: true });
+
+const json = require(inputFile);
+const csv = json2csv(json);
+const charset = 'utf8';
+fs.writeFileSync(outputFile, csv, charset);
